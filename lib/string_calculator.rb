555ee3690fea3c6e6c8ffa134f716ca8719fea6f
Krishnamurthi Srikumar <shrikrishnamurthi@gmail.com>
2025-05-02 15:10:13 +0530

    feat: add support for summing two comma-separated numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,12 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers == ""
+
+    if numbers.include?(",")
+      parts = numbers.split(",").map(&:to_i)
+      return parts.sum
+    end
+    
     return numbers.to_i
   end
 end
