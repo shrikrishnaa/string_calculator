e55cff707745ed74d481fa861456fe7d5828826a
Krishnamurthi Srikumar <shrikrishnamurthi@gmail.com>
2025-05-02 15:30:17 +0530

    feat: handle newline characters in the input string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,11 +2,9 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers == ""
 
-    if numbers.include?(",")
-      parts = numbers.split(",").map(&:to_i)
-      return parts.sum
-    end
-    
-    return numbers.to_i
+    numbers = numbers.gsub("\n", ",")
+
+	parts = numbers.split(",").map(&:to_i)
+	parts.sum
   end
 end
